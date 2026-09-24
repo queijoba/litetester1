b3bb7785676c92c3f0c329fab5f2617d404ceffe
Nick <90225335+queijoba@users.noreply.github.com>
2026-09-24 18:32:59 -0300

    feat: install Dragonbane editable PDF export button

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import './pjlite.css';
 import PJLiteApp from './PJLiteApp.jsx';
+import { installDragonbanePdfExport } from './pdf/dragonbanePdfExport.js';
 
 createRoot(document.getElementById('root')).render(<PJLiteApp />);
+installDragonbanePdfExport();
