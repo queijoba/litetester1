f72ae76735f7bc37b9274f68fde958121ef4f377
Nick <90225335+queijoba@users.noreply.github.com>
2026-09-25 02:01:19 -0300

    Usa módulo organizado de PDF Dragonbane

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import './pjlite.css';
 import PJLiteApp from './PJLiteApp.jsx';
-import { installDragonbanePdfExport } from './pdf/dragonbanePdfExportTemplate.js';
+import { installDragonbanePdfExport } from './features/pdf/dragonbane/export.js';
 
 createRoot(document.getElementById('root')).render(<PJLiteApp />);
 installDragonbanePdfExport();
