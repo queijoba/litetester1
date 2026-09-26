899ef3ccbe4a00bf9215fcfa689c4207313e1304
Nick <90225335+queijoba@users.noreply.github.com>
2026-09-26 08:50:07 -0300

    Carrega refinamento mobile dos sistemas

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import './pjlite.css';
 import PJLiteApp from './PJLiteApp.jsx';
 import './mobile-polish.css';
+import './mobile-systems-v8.css';
 import { installDragonbanePdfExport } from './systems/dragonbane/index.js';
 
 createRoot(document.getElementById('root')).render(<PJLiteApp />);
