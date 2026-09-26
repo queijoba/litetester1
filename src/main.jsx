3b7eab97646ca07f7855f01cde36ecb90f39578c
Nick <90225335+queijoba@users.noreply.github.com>
2026-09-26 08:31:17 -0300

    Carrega polimento mobile final

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import './pjlite.css';
 import PJLiteApp from './PJLiteApp.jsx';
+import './mobile-polish.css';
 import { installDragonbanePdfExport } from './systems/dragonbane/index.js';
 
 createRoot(document.getElementById('root')).render(<PJLiteApp />);
